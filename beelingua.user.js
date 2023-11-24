6fa67a33f727657060649dc9fcd9c4d9aa163db4
Kai Folf <git@fcd.im>
2023-11-24 21:56:27 +0700

    Update beelingua.user.js

--- beelingua.user.js
+++ beelingua.user.js
@@ -3,7 +3,7 @@
 // @namespace    https://github.com/folfcoder/beelingua
 // @downloadURL  https://github.com/folfcoder/beelingua/raw/main/beelingua.user.js
 // @updateURL    https://github.com/folfcoder/beelingua/raw/main/beelingua.user.js
-// @version      0.2
+// @version      0.3
 // @description  Show answer keys for Beelingua.
 // @author       Kai Folf
 // @match        https://newbinusmaya.binus.ac.id/beelingua/*
@@ -17,13 +17,13 @@
     const ENDPOINT = "https://beelingua.folfcoder.workers.dev";
 
     // Variables
-    let lang, course, unit, number;
+    let lang, course, unit, title, number;
 
     // Function to extract information from the page
     function extractInfo() {
         const unitElem = document.querySelector("h5");
         const unitText = unitElem.textContent;
-        GM_log(unitText);
+        title = document.querySelector("h1").textContent;
 
         const [langPart, titlePart, unitPart] = unitText.split(" - ");
         lang = langPart.split("-")[0].trim();
@@ -33,6 +33,7 @@
         GM_log("[DEBUG] Bahasa: " + lang);
         GM_log("[DEBUG] Course: " + course);
         GM_log("[DEBUG] Unit: " + unit);
+        GM_log("[DEBUG] Title: " + title);
     }
 
     // Function to fetch and highlight answers
@@ -44,16 +45,26 @@
                 number = newNum;
                 GM_log("[DEBUG] Nomor: " + number);
 
-                fetch(`${ENDPOINT}/${lang}/${course}/${unit}/${number}`)
+                fetch(`${ENDPOINT}/${lang}/${course}/${unit}/${title}/${number}`)
                     .then((response) => response.json())
                     .then((data) => {
                         GM_log("[DEBUG] Jawaban: " + data);
+                                            // Multiple choice
                         const options = document.querySelectorAll(".bl-content-center");
                         options.forEach((option) => {
                             if (data.includes(option.textContent)) {
                                 option.style.border = "0.3rem solid rgb(255, 0, 255)";
                             }
                         });
+
+                        // Text
+                        if (data[0].length > 1) {
+                            const textarea = document.querySelector('textarea[placeholder="Type your answer here"]');
+                            if (textarea) {
+                                textarea.placeholder="Beelingua Helper: Answer copied to clipboard!";
+                            }
+                            navigator.clipboard.writeText(data[0]);
+                        }
                     });
             }
         });
